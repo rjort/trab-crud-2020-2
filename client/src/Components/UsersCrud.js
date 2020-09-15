28aabb4b5bee561eca842388a4097c702ca33f0c
Reuter Junior <reuter_junior@hotmail.com>
2020-09-15 17:02:35 -0300

    adicionado funcoes de interacoes: delete, update e create

--- client/src/Components/UsersCrud.js
+++ client/src/Components/UsersCrud.js
@@ -1,53 +1,94 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import MaterialTable from 'material-table'
 
+import Axios from 'axios'
+
 export default function UserTable() {
-  const [state, setState] = React.useState({
-    columns: [
-      { title: 'Name', field: 'name' },
-      { title: 'Email', field: 'email' },
-      { title: 'Phone', field: 'phone' }
-    ],
-    data: []
-  })
+  const baseURL = `http://localhost:5000/api/v1/users`
+  const [col] = useState([
+    { title: 'Name', field: 'name' },
+    { title: 'Email', field: 'email' },
+    { title: 'Phone', field: 'phone' }
+  ])
+
+  const [data, setData] = useState([])
+
+  useEffect( () => {
+    async function fetchData() {
+      const res = await Axios.get(baseURL)
+      setData(res.data)
+    }
+    fetchData()
+  }, [])
+
+  const createUser = (newUser) => {
+    async function fetchUser() {
+      console.log(newUser)
+      const res = await Axios.post(baseURL, newUser)
+      if(res.status === 200) return res.data
+    }
+    fetchUser()
+  }
+
+  const deleteUser = (user) => {
+    async function fetchUser() {
+      console.log(user._id)
+      const res = await Axios.delete(`${baseURL}/${user._id}`, user)
+      if(res.status === 200) return res.data
+    }
+    fetchUser()
+  }
+
+  const updateUser = (oldUser, newUser) => {
+    async function fetchUser() {
+      console.log(oldUser)
+      const res = await Axios.put(`${baseURL}/${oldUser._id}`, newUser)
+      console.log(res.data)
+      if(res.status === 200) return res.data
+    }
+    fetchUser()
+  }
 
   return(
     <MaterialTable
      title="Users List"
-     columns={state.columns}
-     data={state.data}
+     columns={col}
+     data={data}
      editable={{
-       onRowAdd: (newData) => new Promise((resolve) => {
-         setTimeout(() => {
-           resolve()
-           setState((prevState) => {
-             const data = [...prevState.data]
-             data.push(newData)
-             return { ...prevState, data }
-           })
-         }, 600)
-       }),
-       onRowUpdate: (newData, oldData) => new Promise((resolve) => {
+      onRowAdd: (newData) => new Promise((resolve) => {
+          newData = createUser(newData)
+          setTimeout(() => {
+            resolve()
+            setData((prevState) => {
+              const data = [...prevState]
+              if(newData !== null && newData !== undefined) return data.push(newData)
+              return data
+            })
+          }, 600)
+      }),
+      onRowUpdate: (newData, oldData) => new Promise((resolve) => {
+        updateUser(oldData, newData)
         setTimeout(() => {
           resolve()
           if(oldData) {
-            setState((prevState) => {
-              const data = [...prevState.data]
+            setData((prevState) => {
+              const data = [...prevState]
               data[data.indexOf(oldData)] = newData
-              return { ...prevState, data }
+              return data
             })
           }
         }, 600)
-       }),
-       onRowDelete: (oldData) => new Promise((resolve) => {
-         setTimeout(() => {
-           resolve()
-           setState((prevState) => {
-             const data = [...prevState.data]
-             data.splice(data.indexOf(oldData), 1)
-             return { ...prevState, data }
-           })
-         })
+      }),
+      onRowDelete: (oldData) => new Promise((resolve) => {
+        deleteUser(oldData)
+        setTimeout(() => {
+          resolve()
+          setData((prevState) => {
+            const data = [...prevState]
+            data.splice(data.indexOf(oldData), 1)
+            return data
+          })
+         }, 600)
        })
      }}
      ></MaterialTable>
